ff68798f6581459f8f43d89e924a5d8cc54c9479
Guilherme Patriarca <gpatriarca@ubiwhere.com>
2019-12-17 12:22:03 +0000

    fix: add environments

--- src/environments/docker.js
+++ src/environments/docker.js
@@ -1,8 +1,8 @@
-export const API_BASE_URL = `${process.env.API_BASE_URL}`
+export const API_BASE_URL = `https://5g-dashboard.i2cat.net`
 export const API_SDK = `${process.env.API_BASE_URL}/gw/sdkapi`
 export const API_SLICE_MANAGEMENT = `${process.env.API_BASE_URL}/gw/slicemanagerapi`
+export const API_ALERTS = `${process.env.API_BASE_URL}/gw/5gcityalertapi`
 export const MEDIA_BASE_URL = `https://api.development.test`
 export const SENTRY_URL = `https://eab890d8dc9d4493ae2db1a4d5d45b69@sentry2.ubiwhere.com/56`
 export const LOCATION = [ 2.175125, 41.386707 ]
-// export const API_MONITORING = `${process.env.API_MONITORING}`
 export const API_MONITORING = `http://84.88.37.165:3000`
